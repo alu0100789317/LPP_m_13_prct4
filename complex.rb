b5dff3acba2eed2a1b9d2a38b58ba3bca7b16152
Jorge Machín <alu0100789317@ul.edu.es>
2014-10-17 22:10:19 +0100

    Escribiendo programa complejox.rb

---
+++ complex.rb
@@ -0,0 +1,32 @@
+class Complex
+
+	attr_reader :Re, :Im
+	def initialize(Re, Im)
+		@Re, @Im = Re, Im
+	end
+	def to_s
+		"(#{@Re}+#{@Im}i)"
+	end
+	def + (complejo)
+
+		Complex.new(complejo.Re + @Re , complejo.Im + @Im)
+	end
+	def * (complejo)
+
+		Complex.new(complejo.Re * @Re - complejo.Im * @Im , complejo.Re * @Im + complejo.Im * @Re)
+	end
+	def / (complejo)
+
+		Complex.new((complejo.Re * @Re + complejo.Im * @Im) / (@Im^2 + @Re^2) , (complejo.Im * @Re + complejo.Re * @Im) / (@Im^2 + @Re^2))
+	end
+	def *(num)
+
+		Complex.new( num * @Re , num * @Im)
+
+	end
+end
+		
+
+
+
+		
